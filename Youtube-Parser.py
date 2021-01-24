f3142b6caf35ac862c8a65698a3d0153f2b150eb
Justin Jiang <justinjiang37@gmail.com>
2021-01-23 22:57:52 -0800

    Input UI changes added, added scroll bar

--- Youtube-Parser.py
+++ Youtube-Parser.py
@@ -54,7 +54,7 @@ def YoutubeParser(keywords, link):
         print("========================")
         driver.get(link.get())
 
-        for item in range(5):  # by increasing the highest range you can get more content
+        for item in range(10):  # by increasing the highest range you can get more content
             wait.until(EC.visibility_of_element_located(
                 (By.TAG_NAME, "body"))).send_keys(Keys.END)
             time.sleep(1)
@@ -62,22 +62,52 @@ def YoutubeParser(keywords, link):
         root = tk.Tk()
         root.title("ALL Comments")
 
+        # creating out put frame
+        OutPut_Frame_1 = Frame(root)
+        OutPut_Frame_1.pack(fill = BOTH, expand = 1)
+
+        # creating out put canvas in side frame
+        OutPut_Canvas = Canvas(OutPut_Frame_1)
+        OutPut_Canvas.pack(side = LEFT, fill = BOTH, expand = 1)
+
+        # adding the scroll bar
+        scrollBar = ttk.Scrollbar(OutPut_Frame_1, orient = VERTICAL, command = OutPut_Canvas.yview)
+        scrollBar.pack(side = RIGHT, fill = Y)
+
+        # configuring canvas
+        OutPut_Canvas.configure(yscrollcommand = scrollBar.set)
+        OutPut_Canvas.bind('<Configure>', lambda e: OutPut_Canvas.configure(
+            scrollregion = OutPut_Canvas.bbox("all")))
+
+        # create new frame in side canvas
+        OutPut_Frame_2 = Frame(OutPut_Canvas)
+
+        # add second frame in canvas
+        OutPut_Canvas.create_window((0,0), window = OutPut_Frame_2, anchor = "nw")
+
+
         for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#comment #content-text"))):
-            # Try running without if and just add all.
             words = []
-            tmp = ""
+            temp_sentence = ""
             punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
             for i in comment.text:
                 if i not in punctuations:
-                    tmp = tmp + i
-            words = tmp.split()
+                    temp_sentence = temp_sentence + i
+
+            words = temp_sentence.split()
             for j in range(len(words)):
                 words[j] = words[j].lower()
+
             if any(temp in words for temp in keywords):
-                text = tk.Label(root, text=comment.text)
+                temp_comment = comment.text
+                for i in range(len(temp_comment)):
+                    if i % 65 == 0:
+                        temp_comment = temp_comment[:i] + "\n" + temp_comment[i:]
+                text = tk.Label(OutPut_Frame_2, text=temp_comment)
                 text.pack()
                 space = tk.Label(
-                    root, text="=====================================")
+                    OutPut_Frame_2, text="=====================================")
                 space.pack()
 
         root.mainloop()
@@ -92,32 +122,32 @@ def UI():
     init.pack()
 
     # keywords
-    keywordTextBox = tk.Canvas(root, width=400, height=300)
-    keywordTextBox.place(relx=0.5, rely=0.3, anchor='center')
+    keywordTextBox = tk.Canvas(root, width=300, height=80)
+    keywordTextBox.place(relx=0.5, rely=0.45, anchor='center')
     keywordTextBox.configure(bg="white")
     keywordTextBox.config(borderwidth=0)
 
     img = PhotoImage(file="C:/Users/Justi/Keyword.png")
     keyboardLabel = tk.Label(root, image=img)
     keyboardLabel.config(borderwidth=0)
-    keyboardLabel.place(relx=0.34, rely=0.4175, anchor='center')
+    keyboardLabel.place(relx=0.365, rely=0.449, anchor='center')
 
     keywords = tk.Entry(root)
-    keywordTextBox.create_window(200, 225, window=keywords)
+    keywordTextBox.create_window(170, 40, window=keywords)
 
     # link
-    linkTextBox = tk.Canvas(root, width=400, height=200, borderwidth=0)
-    linkTextBox.place(relx=0.5, rely=0.75, anchor='center')
+    linkTextBox = tk.Canvas(root, width=300, height=80, borderwidth=0)
+    linkTextBox.place(relx=0.5, rely=0.60, anchor='center')
     linkTextBox.configure(bg="white")
     linkTextBox.config(borderwidth=1)
 
     img1 = PhotoImage(file="C:/Users/Justi/link.png")
     linkLabel = tk.Label(root, image=img1)
-    linkLabel.place(relx=0.36, rely=0.58, anchor='center')
+    linkLabel.place(relx=0.38, rely=0.595, anchor='center')
     linkLabel.config(borderwidth=0)
 
     link = tk.Entry(root)
-    linkTextBox.create_window(200, 0, window=link)
+    linkTextBox.create_window(170, 40, window=link)
 
     # button
     button1 = tk.Button(text='Get all comments',
@@ -128,9 +158,9 @@ def UI():
     button1.place(relx=0.5, rely=0.75, anchor='s')
 
     # title
-    titleImg = PhotoImage(file="C:/Users/Justi/title.png")
-    titleLabel = tk.Label(root, image=titleImg)
-    titleLabel.place(relx=0.5, rely=0.25, anchor='center')
+    titleLabel = tk.Label(root, text = "Welcome to the YouTube Comment Parser")
+    titleLabel.config(font=("Helvetica", 15))
+    titleLabel.place(relx=0.5, rely=0.3, anchor='center')
     root.mainloop()
 
 
